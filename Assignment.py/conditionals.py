7183c31ab5984d43e17b53c71a5f31c2a4e1baad
Dayo <ifedayoomole0@gmail.com>
2023-05-16 07:40:04 -0700

    assignment solutions

---
+++ Assignment.py/conditionals.py
@@ -0,0 +1,82 @@
+1. 
+user_input= (input["Enter your age:" ])
+if user_input >=18:
+    print("You are old enough to drive")
+elif user_input <18:
+    print("individuals below the age of 18, cannot learn how to drive ")
+    
+    
+
+3. 
+a = 6
+b = 10
+if a>b: 
+    print("a is greater than b")
+elif a<b:
+    print("a is smaller than b")
+else: a=b
+print("a is equal to b")
+
+
+4. 
+
+    
+5. 
+season = input("Season:" )
+if season is Autumn:
+    print("Month: September, October or November")
+if season is Winter:
+    print("Month: December, January or February")
+if season is spring:
+    print("Month: March, April or May")
+if season is Summer:
+    print("Month: June, July or August")
+else:
+    print("season can not be checked")
+
+
+6. 
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.append ( 'watermelon')
+fruits.sort()
+print(fruits)
+['banana', 'lemon', 'mango', 'orange', 'watermelon']
+
+7. 
+person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_marred': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+    }
+person['profession']= 'tech instructor'
+print(person)
+
+print(person['skills'])
+print(person['skills'.value(2)])
+
+print(person['skills'])
+print(person['skills'.value(4)])
+
+if skills ==  ['JavaScript' and 'React'] :
+    print ('He is a front end developer')
+if skills ==  ['Node', 'Python', 'MongoDB']: 
+    print('He is a backend developer')
+if skills == ['React', 'Node' and 'MongoDB']:
+    Print('He is a fullstack developer')
+else:
+    print('unknown title')
+
+
+
+
+
+
+    
+
