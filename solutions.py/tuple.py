610c4239913653dfd5c1f7b8f67d44f24862e779
Dayo <ifedayoomole0@gmail.com>
2023-05-20 06:27:55 -0700

    solutions

---
+++ solutions.py/tuple.py
@@ -0,0 +1,14 @@
+1. 
+city = () 
+2. 
+_sisters = ('Kemi', 'Annora', 'Solange')
+_Brothers = ('John', 'Harry', 'Hamiltion')
+siblings = _sisters + _Brothers
+print (siblings)
+len (siblings)
+parents_= ('David', 'wummi')
+family_members = (parents_ ,siblings)
+print(family_members)
+
+
+ 
