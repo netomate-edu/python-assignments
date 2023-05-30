22d4f4377550d8e3a3443461d3435481515ede3d
Dayo <ifedayoomole0@gmail.com>
2023-05-30 12:33:38 -0700

    added conditional,operator, variables.py files

---
+++ solutions.py/operators.py
@@ -0,0 +1,105 @@
+1.
+age= 20
+2. 
+height= 1.73
+3. 
+complex (18+3j)
+
+4.
+# area of a traingle 
+base= input("Enter Base: ")
+height= input("Enter Height: ")
+area= base * height * 0.5
+print( 'the area of the triangle is:' )
+
+5.
+# perimeter of a triangle
+a= input('enter side a:')
+b= input('enter side b:')
+c= input('enter side c:')
+
+perimeter = a + b + c
+print('perimeter is: ')
+
+
+6.
+# area, perimeter of a rectangle
+length= float(input("Enter Length: "))
+
+width= float(input("Enter Width: "))
+
+area= width  * length
+perimeter =length + width
+print('area of a rectangle:', area)
+print('perimeter of a rectangle:', perimeter)
+
+7. 
+# radius, cirumference of a circle
+pi= 3.142
+radius= float(input("Enter Radius: "))
+area= pi * radius * radius
+circumference= 2 * pi * radius
+print('area of a circle', area)
+print('circumference of a circle', circumference)
+
+
+12. 
+program_language_A ='python'
+program_language_B = 'jargon'
+len(program_language_A)
+6
+len(program_language_B)
+6
+# comparison operators
+print(program_language_A > program_language_B)
+# true
+print(program_language_A == program_language_B)
+# false
+print(program_language_A != program_language_B)
+# true
+print(program_language_A < program_language_B)
+# false
+print(program_language_A >= program_language_B)
+# true
+print(program_language_A <= program_language_B)
+# false
+
+18. 
+a= 7
+b= 3
+print('floor Division:',  a // b)
+# 2
+
+19.
+('10' == 10)
+false
+
+20. 
+int('9.8' == 10)
+0
+
+21.
+hours= float(input("Enter Hours: "))
+rate_per_hour= float(input("Enter Rate: "))
+earning= (Hours * Rate)
+print( 'your weekly earning is' %aearning )
+
+22. 
+years= float(input("Enter the number of years you have lived: "))
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+  
